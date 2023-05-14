9a5c0e6d234d59387ec3b22041a89aae1a9a78ee
Kevin <kevin@Kevins-MacBook-Air.local>
2023-05-15 01:42:33 +0800

    feature: able to upload multiple files properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-from typing import List
+from typing import Annotated, List
 import boto3
 from config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_BUCKET_NAME
 
@@ -17,29 +17,18 @@ app.add_middleware(
 
 
 s3 = boto3.client('s3',
-                  aws_access_key_id='YOUR_ACCESS_KEY_ID',
-                  aws_secret_access_key='YOUR_SECRET_ACCESS_KEY')
-
-
-@app.get("/")
-def read_root():
-    return {"AWS_ACCESS_KEY_ID": AWS_ACCESS_KEY_ID,
-            "AWS_SECRET_ACCESS_KEY": AWS_SECRET_ACCESS_KEY,
-            "AWS_BUCKET_NAME": AWS_BUCKET_NAME}
+                  aws_access_key_id=AWS_ACCESS_KEY_ID,
+                  aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
 
 @app.get("/healthcheck")
 def health_check():
-    return {}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+    return {"status": "ok"}
 
 
 @app.post("/uploadfiles/")
 async def create_upload_files(files: List[UploadFile] = File(...)):
     for file in files:
-        s3.upload_fileobj(file.file, 'YOUR_BUCKET_NAME', file.filename)
-    return {"files_uploaded": [file.filename for file in files]}
+        key = f"data/{file.filename}"
+        s3.upload_fileobj(file.file, AWS_BUCKET_NAME, key)
+    return {"files_uploaded": [f"data/{file.filename}" for file in files]}
